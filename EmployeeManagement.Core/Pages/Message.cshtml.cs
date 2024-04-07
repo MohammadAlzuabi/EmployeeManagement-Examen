9282e54596484df94a79955a3cb1f97f0b1eb1b3
Mohammad Alzuabi <mhamadzu12@hotmail.com>
2024-04-07 19:06:46 +0200

    fix message

--- EmployeeManagement.Core/Pages/Message.cshtml.cs
+++ EmployeeManagement.Core/Pages/Message.cshtml.cs
@@ -15,51 +15,59 @@ namespace EmployeeManagement.Core.Pages
         public string StatusMessage { get; set; }
 
 
-        public List<Models.User> Users { get; set; }
         [BindProperty]
-        public Message SendMessage { get; set; }
+        public Models.Message SendMessage { get; set; }
         public int UserId { get; set; }
-        public User CreatedByUser { get; set; }
+        public User SendToUser { get; set; }
 
         public MessageModel(HttpService httpService, ModelManagement modelManagement)
         {
             _httpService = httpService;
             _modelManagement = modelManagement;
         }
+
         public async Task<IActionResult> OnGetAsync(int userId)
         {
-            var user = await _httpService.HttpGetRequest<Models.User>($"User/{userId}");
-            CreatedByUser = await _httpService.HttpGetRequest<Models.User>($"User/{1}");
-            Users = await _modelManagement.UpdateListAsync<Models.User>();
-
-            if (userId != 0)
-            {
-                UserId = userId;
-            }
-            if (user == null || CreatedByUser == null) return NotFound();
+            await SetUserData(userId);
             return Page();
         }
-
-        public async Task<IActionResult> OnPostSendMessage(int userId)
+        public async Task<IActionResult> OnPostAsync(int userId)
         {
+            await SetUserData(userId);
             var user = UserManagement.GetLoggedInUser();
-            CreatedByUser = await _httpService.HttpGetRequest<Models.User>($"User/{1}");
 
             if (user != null)
             {
                 UserId = user.Id;
             }
             SendMessage.SentAt = DateTime.Now;
-            SendMessage.ToUserId = CreatedByUser.Id;
+            SendMessage.ToUserId = SendToUser.Id;
             SendMessage.FromUserId = UserId;
-            if (SendMessage.Id != 0 && SendMessage.FromUserId != 0 && SendMessage.ToUserId != 0 && (SendMessage.Content != null || SendMessage.Content != string.Empty))
+            if (SendMessage.FromUserId != 0 && SendMessage.ToUserId != 0 && (SendMessage.Content != null || SendMessage.Content != string.Empty))
             {
                 await _httpService.HttpPostRequest($"Message", SendMessage);
-                return RedirectToPage("./Index");
+                return RedirectToPage("./Message");
 
             }
             StatusMessage = "Failed to send message!";
             return Page();
+        }
+
+
+
+        private async Task SetUserData(int userId)
+        {
+            var user = await _httpService.HttpGetRequest<Models.User>($"User/{userId}");
+            SendToUser = await _httpService.HttpGetRequest<Models.User>($"User/{1}"); // TODO: send to admin 
+
+            if (userId != 0)
+            {
+                UserId = userId;
+            }
+            if (user == null || SendToUser == null)
+            {
+                throw new InvalidOperationException("User or CreatedByUser not found");
+            }
         }
     }
 
